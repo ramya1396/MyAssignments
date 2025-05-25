664f09a99a392c412bc9ba0cc3742f2ebd9cc524
ramya1396 <melwinramya@gmail.com>
2025-05-25 08:09:53 +0530

    Add files via upload
    Day 1

---
+++ week1/day1/Chrome.java
@@ -0,0 +1,26 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+	
+		//declared the variables and print it in console
+		float version = 91.0F;
+		System.out.println("The Version is"+ " "+ version);
+		
+		String developer= "Google";
+		System.out.println("The Developer is"+ " "+developer);
+		
+		int releaseYear=2008;
+		System.out.println("The release year is"+" "+releaseYear);
+		
+		char shortCutKey='C';
+		System.out.println("The shortCutKey is" +" "+shortCutKey);
+		
+		boolean isBeta=false;
+		System.out.println("Is This a Beta version"+" "+isBeta);
+		
+
+	}
+
+}
